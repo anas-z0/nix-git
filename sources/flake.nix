03c173f9e1b708c33b94c2d3875ec0118da7777b
anas-z0 <anas-z0@users.noreply.github.com>
2025-12-27 15:10:04 +0300

    test

--- sources/flake.nix
+++ sources/flake.nix
@@ -8,6 +8,31 @@
       flake = false;
       url = "gitlab:wlroots/wlroots?host=gitlab.freedesktop.org";
     };
+    "bees.src" = {
+      flake = false;
+      url = "github:Zygo/bees";
+    };
+    "alacritty.src" = {
+      flake = false;
+      url = "github:alacritty/alacritty";
+    };
+    "alacritty-graphic.src" = {
+      flake = false;
+      url = "github:ayosec/alacritty";
+    };
+    "ananicy-rules-cachyos.src" = {
+      flake = false;
+      url = "github:CachyOS/ananicy-rules";
+    };
+    "ananicy-cpp.src" = {
+      flake = false;
+      url = "github:ananicy-cpp/ananicy-cpp";
+    };
+    "appmenu-glib-translator.src" = {
+      flake = false;
+      url = "github:vala-panel-project/vala-panel-appmenu";
+    };
+
   };
   outputs = {...}: {};
 }
